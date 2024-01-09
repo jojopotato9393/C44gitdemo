2edeebe3873a011d47dd63098956660db102974f
[Joseph Gaughan] <[jojopotato9393@gmail.com]>
2024-01-09 12:55:59 -0600

    wake up stevie

---
+++ wake-him-up/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SleepySloth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stevie = "what does stevie need to wake up?";
+            Console.WriteLine(stevie);
+            string needed = Console.ReadLine();
+            Console.WriteLine("All Stevie needed was some " + needed + "!!" );
+        }
+    }
+}
