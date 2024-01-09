246d120d46a09150c9404cf49172b5f1f3fb4726
[Joseph Gaughan] <[jojopotato9393@gmail.com]>
2024-01-09 10:31:38 -0600

    second line

---
+++ program.cs
@@ -0,0 +1,15 @@
+useing System;
+
+namespace testcode
+{
+class program
+{
+static void Main(string[] agrs)
+{
+string opening = "is codeing fun?";
+Console.WriteLine(opening);
+string response = Console.ReadLine("your awnser" ());
+Console.ReadLine(opening + response);
+}
+}
+}
